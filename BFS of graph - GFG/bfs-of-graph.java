284b7bb0da249e3503494c23332bc948697e959c
svidhawan <68031319+svidhawan@users.noreply.github.com>
2022-06-06 14:50:49 +0530

    Added solution - LeetHub

---
+++ BFS of graph - GFG/bfs-of-graph.java
@@ -0,0 +1,66 @@
+// { Driver Code Starts
+// Initial Template for Java
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            String[] s = br.readLine().trim().split(" ");
+            int V = Integer.parseInt(s[0]);
+            int E = Integer.parseInt(s[1]);
+            ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+            for (int i = 0; i < V; i++) adj.add(i, new ArrayList<Integer>());
+            for (int i = 0; i < E; i++) {
+                String[] S = br.readLine().trim().split(" ");
+                int u = Integer.parseInt(S[0]);
+                int v = Integer.parseInt(S[1]);
+                adj.get(u).add(v);
+                // adj.get(v).add(u);
+            }
+            Solution obj = new Solution();
+            ArrayList<Integer> ans = obj.bfsOfGraph(V, adj);
+            for (int i = 0; i < ans.size(); i++)
+                System.out.print(ans.get(i) + " ");
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    // Function to return Breadth First Traversal of given graph.
+    public ArrayList<Integer> bfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
+        // Code here
+        ArrayList<Integer> answer = new ArrayList<>();
+        Queue<Integer> sets= new LinkedList<>();
+        
+        boolean visited[] = new boolean[V];
+        
+        sets.add(0);
+        
+        while(!sets.isEmpty())
+        {
+            int currentElement =sets.remove();
+            
+            if(visited[currentElement])
+            {
+                continue;
+            }
+            else
+            {
+                visited[currentElement]= true;
+                answer.add(currentElement);
+            }
+            for(int neighbours: adj.get(currentElement))
+            {
+                sets.add(neighbours);
+            }
+        }
+        return answer;
+    }
+}
